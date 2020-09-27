2c35cb46573cec81218994a8e6d249148c699881
Shubham <shubham.mehta9999@gmail.com>
2020-09-27 14:30:04 -0600

    Add semesters completed

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ const { ipcRenderer } = require('electron');
 const semester = document.getElementById('semesterIcon');
 const course = document.getElementById('courseIcon');
 const assignment = document.getElementById('assignmentIcon');
+const settings = document.getElementById('settingsIcon');
 
 semester.addEventListener('click', () => {
     ipcRenderer.send('semester-clicked');
@@ -14,4 +15,8 @@ course.addEventListener('click', () => {
 
 assignment.addEventListener('click', () => {
     ipcRenderer.send('assignment-clicked');
+});
+
+settings.addEventListener('click', () => {
+    ipcRenderer.send('setting-clicked');
 });
