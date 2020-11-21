2419be572138485951e6e5c153d76b887321a681
sam2127 <61511436+sam2127@users.noreply.github.com>
2020-11-21 14:49:25 -0600

    Made minor changes to interface. (#87)

--- clean-data.js
+++ clean-data.js
@@ -5,76 +5,98 @@ const semesters = [];
 const courses = [];
 const assignments = [];
 const settings = {};
-let failure = false;
+const settingsFile = path.join(__dirname, "./src/.config/settings.json");
 
+let failure = false;
+let configuration;
+
+// Run cleanAll().
 cleanAll();
 
 function cleanAll() {
-    cleanSemesters();
-    cleanCourses();
-    cleanAssignments();
-    cleanSettings();
 
-    if (failure) {
-        console.log("There was a failure during clean operation. Clean manually before commit/push.");
+    readConfiguration();
+
+    // When settings are not empty and we have values for all config files and folders.
+    if (Object.keys(configuration).length != 0) {
+        cleanSemesters();
+        cleanCourses();
+        cleanAssignments();
+        cleanSettings();
+
+        if (failure) {
+            console.error("There was a failure during clean operation. Clean manually before commit/push.");
+        } else {
+            console.log("Success! Project is clean now. You can proceed with commit/push.");
+        }
     } else {
-        console.log("Success! Project is clean now. You can proceed with commit/push.");
+        console.log("Project is in clean state already. Nothing to do.");
+    }
+
+}
+
+function readConfiguration() {
+    try {
+        // Read configuration from settings file.
+        configuration = JSON.parse(fs.readFileSync(settingsFile, "utf-8"));
+    } catch (error) {
+        console.error('Error!', "Error reading settings file.");
     }
 }
 
 function cleanSemesters() {
-    
+
     try {
-        let semestersFile = path.join(__dirname, "./src/.config/semesters.json");
+        let semestersFile = path.join(configuration.configRootFolder, configuration.semestersDataFile);
         fs.writeFileSync(semestersFile, JSON.stringify(semesters, null, 4));
         console.log("Success! Semesters file is empty now.");
     } catch (error) {
-        console.log("Cleaning of semesters data failed.");
+        console.error("Cleaning of semesters data failed.");
         failure = true;
     }
 }
 
 function cleanCourses() {
-    
+
     try {
-        let coursesFile = path.join(__dirname, "./src/.config/courses.json");
+        let coursesFile = path.join(configuration.configRootFolder, configuration.coursesDataFile);
         fs.writeFileSync(coursesFile, JSON.stringify(courses, null, 4));
         console.log("Success! Courses file is empty now.");
     } catch (error) {
-        console.log("Cleaning of courses data failed.");
+        console.error("Cleaning of courses data failed.");
         failure = true;
     }
 }
 
 function cleanAssignments() {
-    
+
     try {
-        let assignmentsFile = path.join(__dirname, "./src/.config/assignments.json");
+        let assignmentsFile = path.join(configuration.configRootFolder, configuration.assignmentsDataFile);
         fs.writeFileSync(assignmentsFile, JSON.stringify(assignments, null, 4));
         console.log("Success! Assignments file is empty now.");
     } catch (error) {
-        console.log("Cleaning of assignments data failed.");
+        console.error("Cleaning of assignments data failed.");
         failure = true;
     }
 
+    // Delete all assignments.
     try {
-        fs.rmdirSync(path.join(__dirname, "./assignments"), { recursive: true });
+        fs.rmdirSync(configuration.assignmentRootFolder, { recursive: true });
         console.log("Success! Assignment files and directories deleted.");
     } catch (error) {
-        console.log(error);
-        console.log("FAIL!!! Cleaning of assignments files and directories failed. Check you have node v12 or higher installed.")
+        console.error(error);
+        console.error("FAIL!!! Cleaning of assignments files and directories failed. Check you have node v12 or higher installed.")
         failure = true;
     }
 }
 
 function cleanSettings() {
-    
+
     try {
-        let settingsFile = path.join(__dirname, "./src/.config/settings.json");
         fs.writeFileSync(settingsFile, JSON.stringify(settings, null, 4));
         console.log("Success! Settings file is empty now.");
     } catch (error) {
-        console.log("Cleaning of settings failed.");
+        console.error("Cleaning of settings failed.");
         failure = true;
     }
 }
--- src/about.js
+++ src/about.js
@@ -1,11 +1,19 @@
 const { remote, shell } = require("electron");
 const app = remote.app;
 
-const appVersion = "<b>App:</b> v" + app.getVersion();
-const electronVersion = "<b>Electron:</b> v" + process.versions.electron;
-const chromeVersion = "<b>Chrome:</b> v" + process.versions.chrome;
-const nodeVersion = "<b>Node.js:</b> v" + process.versions.node;
-const osVersion = "<b>OS:</b> " + process.env.OS + " v" + process.getSystemVersion();
+const appVersion = app.getVersion();
+const electronVersion = process.versions.electron;
+const chromeVersion = process.versions.chrome;
+const nodeVersion = process.versions.node;
+
+let osVersion;
+
+if (process.platform === "win32" || process.platform === "win64")
+    osVersion = "Windows " + process.getSystemVersion();
+else if (process.platform === "linux")
+    osVersion = "Linux " + process.getSystemVersion();
+else if (process.platform === "darwin")
+    osVersion = "MacOS " + process.getSystemVersion();
 
 document.getElementById("appVersion").innerHTML = appVersion;
 document.getElementById("electronVersion").innerHTML = electronVersion;
